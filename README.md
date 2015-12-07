299c2049ac8ecd0ffd9aade37d57232ff82a6012
Lars Mescheder <larsmescheder@gmx.net>
2015-12-07 16:32:34 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # GenInvGaussian
 
 [![Build Status](https://travis-ci.org/LMescheder/GenInvGaussian.jl.svg?branch=master)](https://travis-ci.org/LMescheder/GenInvGaussian.jl)
+
+This package contains an implementation of the [generalized inverse gaussian distribution](https://en.wikipedia.org/wiki/Generalized_inverse_Gaussian_distribution). Sampling is done by a simple rejection sampling procedure from a gamma distributions. This works well, but naturally this is not the fastest method.
+
+## Usage
+```julia
+using Distributions
+using GenInvGaussian
+
+d = GeneralizedInverseGaussian(1., 10., 10.)
+
+println("Mean = ", mean(d))
+println("Variance = ", var(d))
+println("Mode = ", mode(d))
+
+using PyPlot
+s = rand(d, 1000000)
+plt[:hist](s, bins=100)
+println("Empirical mean = ", mean(s))
+println("Empiricial variance = ", var(s))
+println("Empiricial mode = ", mode(s))
+```
+Output is
+```
+Mean = 1.153417250747945
+Variance = 0.13099554597623
+Mode = 1.0
+Empirical mean = 1.1538116272315135
+Empiricial variance = 0.1308216411777261
+Empiricial mode = 0.9096948416390549
+```
--- src/GenInvGaussian.jl
+++ src/GenInvGaussian.jl
@@ -1,5 +1,96 @@
 module GenInvGaussian
 
-# package code goes here
+export GeneralizedInverseGaussian
+
+import Base.mean
+import Base.rand
+using Distributions
+import Distributions: mean, var, mode, mgf, cf, rand, params
+
+immutable GeneralizedInverseGaussian <: ContinuousUnivariateDistribution
+    p::Float64
+    a::Float64
+    b::Float64
+
+    function GeneralizedInverseGaussian(p::Real, a::Real, b::Real)
+        #@checkargs(GeneralizedInverseGaussian, a > zero(a), b > zero(b))
+        new(p, a, b)
+    end
+end
+
+#@distr_support GeneralizedInverseGaussian 0.0 Inf
+
+#### Parameters
+
+params(d::GeneralizedInverseGaussian) = (d.p, d.a, d.b)
+
+#### Statistics
+
+function mean(d::GeneralizedInverseGaussian)
+    (p, a, b) = params(d)
+    ω = sqrt(a*b)
+    sqrt(b/a) * besselk(p + 1, ω) / besselk(p, ω)
+end
+
+function var(d::GeneralizedInverseGaussian)
+    (p, a, b) = params(d)
+    ω = sqrt(a*b)
+    num = besselk(p, ω)*besselk(p+2, ω) - besselk(p+1, ω)*besselk(p+1, ω)
+    denom = besselk(p, ω) * besselk(p, ω)
+    b/a * num / denom
+end
+
+function mode(d::GeneralizedInverseGaussian)
+    (p, a, b) = params(d)
+    m = (p - 1) + sqrt((p - 1)*(p - 1) + a*b)
+    if m < 0
+        error("Distribution has no mode!")
+    end
+    m / a
+end
+
+function mgf(d::GeneralizedInverseGaussian, t::Real)
+    (p, a, b) = params(d)
+    at = a - 2t
+    ω = sqrt(a*b)
+    ωt = sqrt(at*b)
+    (a/at)^(p/2) * besselk(p, ωt)/besselk(p, ω)
+end
+
+function cf(d::GeneralizedInverseGaussian, t::Real)
+    (p, a, b) = params(d)
+    at = a - 2im * t
+    ω = sqrt(d.a*d.b)
+    ωt = sqrt(at*b)
+    (a/at)^(p/2) * besselk(p, ωt)/besselk(p, ω)
+end
+
+
+#### Evaluation & Sampling
+
+# Simple rejection sampling using a gamma distribution as a proposal distribution.
+# Probably not the best solution out there. However, for p >= 1, the expected number of
+# rejection is lower than sqrt(2).
+function rand(d::GeneralizedInverseGaussian)
+    (p, a, b) = params(d)
+    ω = sqrt(a*b)
+
+    r = p/2 + sqrt(p*p + a*b)/2
+    dproposal = Gamma(r, 2/a)
+    daccept = Uniform()
+
+    xm = b / ( r - p) / 2
+
+    acceptrate(x::Real) = (x./xm)^(p - r) * exp(-b/2*(1./x - 1./xm))
+
+    while true
+        sproposal = rand(dproposal)
+        saccept = rand(daccept)
+
+        if saccept <= acceptrate(sproposal)
+            return sproposal
+        end
+    end
+end
 
 end # module
